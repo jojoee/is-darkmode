be733568476e89fdb1c448e9a4ea65fd64318a5f
Nathachai Thongniran <inid3a@gmail.com>
2020-06-16 15:11:49 +0700

    feat(release): first release

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Check if macOS using Dark appearance
+ *
+ * @returns {Boolean}
+ */
+function isDarkMode () {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+}
+
+/**
+ * Add callback function that's will triggered
+ * when appearance is changed to Dark
+ *
+ * @param {Object} fn function Object
+ * @returns {Boolean} returns true if appearance change to "dark"
+ */
+function onDarkModeChange (fn) {
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    const isDark = e.matches
+    fn(isDark)
+  })
+}
+
+if (typeof module !== 'undefined' &&
+  module.exports != null) {
+  module.exports = {
+    isDarkMode,
+    onDarkModeChange
+  }
+}
---
+++ index.test.js
@@ -0,0 +1,5 @@
+describe('setup', () => {
+  test('true', () => {
+    expect(true).toBeTruthy()
+  })
+})
